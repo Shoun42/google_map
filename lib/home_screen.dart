66c1bcf8d83a3382b2472776e29a6d3d4dc29d6c
Shoun42 <shounbabu31@gmail.com>
2025-03-05 21:20:29 +0600

    05-03-2025

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -86,10 +86,10 @@ class _HomeScreenState extends State<HomeScreen> {
         }),
   };
 
-                  /*পলি লাইনের জন্য*/
+  /*পলি লাইনের জন্য*/
   final Set<Polyline> _polylines = <Polyline>{
     const Polyline(
-      polylineId: PolylineId('My Home to My Institute'),
+      polylineId: PolylineId('My Home to My Institute and another'),
       points: [
         LatLng(25.747399180983482, 89.46055303058475),
         LatLng(26.039013545325922, 88.44284992665052),
@@ -104,50 +104,39 @@ class _HomeScreenState extends State<HomeScreen> {
       endCap: Cap.roundCap,
       width: 4,
       jointType: JointType.round,
-
-
     ),
-
   };
-           /* বৃত্তের জন্য */
-  final Set<Circle> _circle =<Circle>{
+
+  /* বৃত্তের জন্য */
+  final Set<Circle> _circle = <Circle>{
     Circle(
-        circleId: CircleId ('most-denger-Zoon'),
+      circleId: CircleId('most-denger-Zoon'),
       center: LatLng(26.039013545325922, 88.44284992665052),
       radius: 30000,
       fillColor: Colors.red.withOpacity(.3),
       strokeColor: Colors.red.withOpacity(.7),
-      strokeWidth:2,
-
+      strokeWidth: 2,
     ),
   };
 
-             /* পলিগণের জন্য */
+  /* পলিগণের জন্য */
 
-  final Set <Polygon> _polygon =<Polygon>{
+  final Set<Polygon> _polygon = <Polygon>{
     Polygon(
-      polygonId: PolygonId('Polygone-Random'),
-      points: [
-        LatLng(23.948774694599326, 90.34540146589279),
-        LatLng(23.82647018811372, 91.22717499732971),
-        LatLng(23.400835029616665, 91.19051489979029),
-        LatLng(23.227623090705688, 90.59213988482952),
-      ],
-
-      strokeWidth:2,
-      fillColor: Colors.brown.withOpacity(.3),
-      strokeColor: Colors.redAccent,
-
-      onTap: (){
-        print('You Tap on Polygone');
-
-      }
-
-    ),
+        polygonId: PolygonId('Polygone-Random'),
+        points: [
+          LatLng(23.948774694599326, 90.34540146589279),
+          LatLng(23.82647018811372, 91.22717499732971),
+          LatLng(23.400835029616665, 91.19051489979029),
+          LatLng(23.227623090705688, 90.59213988482952),
+        ],
+        strokeWidth: 2,
+        fillColor: Colors.brown.withOpacity(.3),
+        strokeColor: Colors.redAccent,
+        onTap: () {
+          print('You Tap on Polygone');
+        }),
   };
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -164,14 +153,11 @@ class _HomeScreenState extends State<HomeScreen> {
         onMapCreated: (GoogleMapController controller) {
           _mapController = controller;
         },
-
         onTap: (LatLng latLng) {
           print('You Tapped on $LatLng');
         },
-
         onLongPress: (LatLng latLng) {
           print('You long pressed on $LatLng');
-
         },
         trafficEnabled: true,
         zoomControlsEnabled: true,
@@ -183,35 +169,48 @@ class _HomeScreenState extends State<HomeScreen> {
         circles: _circle,
         polygons: _polygon,
       ),
-
-
-      floatingActionButton: Align(
-        alignment: Alignment.bottomLeft ,
-        child: FloatingActionButton(
-          onPressed: _addNewMarker,
-          child: const Icon(Icons.location_on_outlined),
-        ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: _addNewMarker,
+            child: const Icon(Icons.location_on_outlined),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: _goBackToMyLocation,
+            child: const Icon(Icons.my_location),
+          ),
+        ],
       ),
     );
   }
 
   void _addNewMarker() {
-    _marker.add(Marker(
+    _marker.add(
+      Marker(
         markerId: MarkerId('new_marker'),
         position: LatLng(25.7726769782105, 88.9264290779829),
         draggable: true,
-
-        onDragStart:(LatLng startLatLng) {
+        onDragStart: (LatLng startLatLng) {
           print('Start latlng $startLatLng');
-
         },
-
         onDragEnd: (LatLng startLatLng) {
-          print('Stop latlong $startLatLng');},
-
-
-        ),
+          print('Stop latlong $startLatLng');
+        },
+      ),
     );
     setState(() {});
+  }
+
+  void _goBackToMyLocation() {
+    _mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        const CameraPosition(
+          zoom: 7,
+          target: LatLng(25.747399180983482, 89.46055303058475),
+        ),
+      ),
+    );
   }
 }
