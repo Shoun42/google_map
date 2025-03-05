4c5e6b8988c77a621b64b1cbfe68ff8f3ff58cd0
Shoun42 <shounbabu31@gmail.com>
2025-03-05 15:18:51 +0600

    05-03-2025

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -37,29 +37,118 @@ class _HomeScreenState extends State<HomeScreen> {
         onDragEnd: (LatLng startLatLng) {
           print('Stop latlong $startLatLng');
         }),
+
+    //location dinajpur
     Marker(
-      markerId: MarkerId('pick-up-location-marker'),
-      position: LatLng(26.340106384315177, 88.56038365513086),
+        markerId: MarkerId('Friend House'),
+        position: LatLng(25.636718865301358, 88.65111105144024),
+        infoWindow: InfoWindow(title: "Friend House", onTap: () {}),
+        onTap: () {
+          //do whatever you want
+        },
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
+        draggable: true,
+        onDragStart: (LatLng startLatLng) {
+          print('Start latlng $startLatLng');
+        },
+        onDragEnd: (LatLng startLatLng) {
+          print('Stop latlong $startLatLng');
+        }),
 
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      draggable: true,
-    ),
+    Marker(
+        markerId: MarkerId('Friend House'),
+        position: LatLng(25.434305979353898, 89.31283045560122),
+        infoWindow: InfoWindow(title: "Friend House", onTap: () {}),
+        onTap: () {
+          //do whatever you want
+        },
+        icon:
+            BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueMagenta),
+        draggable: true,
+        onDragStart: (LatLng startLatLng) {
+          print('Start latlng $startLatLng');
+        },
+        onDragEnd: (LatLng startLatLng) {
+          print('Stop latlong $startLatLng');
+        }),
 
+    Marker(
+        markerId: MarkerId('pick-up-location-marker'),
+        position: LatLng(26.340106384315177, 88.56038365513086),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+        draggable: true,
+        onDragStart: (LatLng startLatLng) {
+          print('Start latlng $startLatLng');
+        },
+        onDragEnd: (LatLng startLatLng) {
+          print('Stop latlong $startLatLng');
+        }),
   };
 
-  final Set<Polyline> _polylines =<Polyline>{
+                  /*পলি লাইনের জন্য*/
+  final Set<Polyline> _polylines = <Polyline>{
     const Polyline(
       polylineId: PolylineId('My Home to My Institute'),
       points: [
-
         LatLng(25.747399180983482, 89.46055303058475),
         LatLng(26.039013545325922, 88.44284992665052),
 
+        LatLng(25.636718865301358, 88.65111105144024),
 
+        LatLng(25.434305979353898, 89.31283045560122),
+        //LatLng(25.747399180983482, 89.46055303058475), //my home
       ],
-      color: Colors.red
+      color: Colors.red,
+      startCap: Cap.roundCap,
+      endCap: Cap.roundCap,
+      width: 4,
+      jointType: JointType.round,
+
+
+    ),
+
+  };
+           /* বৃত্তের জন্য */
+  final Set<Circle> _circle =<Circle>{
+    Circle(
+        circleId: CircleId ('most-denger-Zoon'),
+      center: LatLng(26.039013545325922, 88.44284992665052),
+      radius: 30000,
+      fillColor: Colors.red.withOpacity(.3),
+      strokeColor: Colors.red.withOpacity(.7),
+      strokeWidth:2,
+
     ),
   };
+
+             /* পলিগণের জন্য */
+
+  final Set <Polygon> _polygon =<Polygon>{
+    Polygon(
+      polygonId: PolygonId('Polygone-Random'),
+      points: [
+        LatLng(23.948774694599326, 90.34540146589279),
+        LatLng(23.82647018811372, 91.22717499732971),
+        LatLng(23.400835029616665, 91.19051489979029),
+        LatLng(23.227623090705688, 90.59213988482952),
+
+      ],
+
+      strokeWidth:2,
+      fillColor: Colors.brown.withOpacity(.3),
+      strokeColor: Colors.redAccent,
+
+      onTap: (){
+        print('You Tap on Polygone');
+
+      }
+
+    ),
+  };
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -70,17 +159,20 @@ class _HomeScreenState extends State<HomeScreen> {
       body: GoogleMap(
         mapType: MapType.terrain,
         initialCameraPosition: const CameraPosition(
-          zoom: 16,
-          target: LatLng(25.747399180983482, 89.46055303058475),
+          zoom: 8.8,
+          target: LatLng(25.747104200515526, 88.93063578754663),
         ),
         onMapCreated: (GoogleMapController controller) {
           _mapController = controller;
         },
+
         onTap: (LatLng latLng) {
           print('You Tapped on $LatLng');
         },
+
         onLongPress: (LatLng latLng) {
           print('You long pressed on $LatLng');
+
         },
         trafficEnabled: true,
         zoomControlsEnabled: true,
@@ -89,20 +181,37 @@ class _HomeScreenState extends State<HomeScreen> {
         compassEnabled: true,
         markers: _marker,
         polylines: _polylines,
+        circles: _circle,
+        polygons: _polygon,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addNewMarker,
-        child: const Icon(Icons.location_on_outlined),
+
+
+      floatingActionButton: Align(
+        alignment: Alignment.bottomLeft ,
+        child: FloatingActionButton(
+          onPressed: _addNewMarker,
+          child: const Icon(Icons.location_on_outlined),
+        ),
       ),
     );
   }
 
   void _addNewMarker() {
-    _marker.add(
-      const Marker(
+    _marker.add(Marker(
         markerId: MarkerId('new_marker'),
-        position: LatLng(26.29384935248975, 88.49854666739702),
-      ),
+        position: LatLng(25.7726769782105, 88.9264290779829),
+        draggable: true,
+
+        onDragStart:(LatLng startLatLng) {
+          print('Start latlng $startLatLng');
+
+        },
+
+        onDragEnd: (LatLng startLatLng) {
+          print('Stop latlong $startLatLng');},
+
+
+        ),
     );
     setState(() {});
   }
